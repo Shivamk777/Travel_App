410904a8b7d6ccf5b7426c4a56212b753f755c35
Shivamk777 <shivampatel17119@gmail.com>
2025-02-06 01:29:37 +0530

    basic front

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <Link href="/"></Link>
+      <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} ></Image>
+    </nav>
   )
 }
 
